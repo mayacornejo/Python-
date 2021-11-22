10d64cf94a79d79e348504eca2a1dd699f3895cf
mayacornejo <94799999+mayacornejo@users.noreply.github.com>
2021-11-21 22:23:46 -0600

    November 21

---
+++ commentDrills.py
@@ -0,0 +1,41 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+'''I really like this video'''
+
+
+
+#2) Make a multi-line comment.
+'''comment here
+#Hello how are you?
+#I am good.
+'''
+
+
+#3) Make a variable. Then put a comment after it, on the same line.
+print(kids)#this prints the variable kids 
+
+
+#4) Make a single line comment.
+'''Ok i will do that'''
+
+#5) Make a multi-line comment.
+'''comment here
+#I will comment on this
+#Oh how nice'''
+
