8b538ceed55f0a37e7baee60eb534d0b5b05d51f
mayacornejo <94799999+mayacornejo@users.noreply.github.com>
2021-11-21 22:13:59 -0600

    November 21

---
+++ commentsOutline.py
@@ -0,0 +1,18 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+'author:Maya'
+
+
+#2) Make a multi-line comment.
+'''comment here'''
+'''My name is Maya'''
+
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+print(students)#this prints the variable studnets 
+
